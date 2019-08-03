d69fc3ce0eb36ded6ac4b02e40748361b1bcd0bf
wangxiayun <wangxiayun@aikucun.com>
2019-08-03 17:11:19 +0800

    CopyOnWriteArrayList

---
+++ src/main/java/com/example/learn/concurrency/c11_copyonwritearraylist/BlackListServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.learn.concurrency.c11_copyonwritearraylist;
+
+import java.util.Map;
+
+/**
+ *应用场景
+ * CopyOnWrite并发容器用于读多写少的并发场景。比如白名单，黑名单，商品类目的访问和更新场景，假如我们有一个搜索网站，用户在这个网站的搜索框中，输入关键字搜索内容，但是某些关键字不允许被搜索。这些不能被搜索的关键字会被放在一个黑名单当中，黑名单每天晚上更新一次。当用户搜索时，会检查当前关键字在不在黑名单当中，如果在，则提示不能搜索。
+ *
+ * 但是使用CopyOnWriteMap需要注意两件事情：
+ 　　1. 减少扩容开销。根据实际需要，初始化CopyOnWriteMap的大小，避免写时CopyOnWriteMap扩容的开销。
+ 　　2. 使用批量添加。因为每次添加，容器每次都会进行复制，所以减少添加次数，可以减少容器的复制次数。如使用上面代码里的addBlackList方法。
+ */
+public class BlackListServiceImpl {
+
+    private static CopyOnWriteMap<String, Boolean> blackListMap = new CopyOnWriteMap<String, Boolean>(1000);
+
+    public static boolean isBlackList(String id) {
+        return blackListMap.get(id) != null;
+    }
+
+    public static void addBlackList(String id) {
+        blackListMap.put(id, Boolean.TRUE);
+    }
+
+    /**
+     * 批量添加黑名单
+     *
+     * @param ids
+     */
+    public static void addBlackList(Map<String,Boolean> ids) {
+        blackListMap.putAll(ids);
+    }
+
+}
